e0ca43a20af896f0115c258f66ac5566db2aa43f
Estudiante <Estudiante@DESKTOP-KS3QO24>
2017-08-30 15:05:11 -0500

    Ejercio1taller1

---
+++ src/libros/Libro.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package libros;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Libro {
+    private String titulo;
+    private String autor;
+    private int numeropaginas;
+    private int calificacion;
+
+    public Libro(String titulo, String autor, int numeropaginas, int calificacion) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numeropaginas = numeropaginas;
+        this.calificacion = calificacion;
+        
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getNumeropaginas() {
+        return numeropaginas;
+    }
+
+    public void setNumeropaginas(int numeropaginas) {
+        this.numeropaginas = numeropaginas;
+    }
+
+    public int getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(int calificacion) {
+        this.calificacion = calificacion;
+    }
+    
+    
+}
